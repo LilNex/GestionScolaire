bab0229b0971441146739363b7fd8ce1c4f21d1f
LilNex <nextorchq@gmail.com>
2021-12-27 07:33:22 +0100

    First Sync

---
+++ GestionScolaire/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace GestionScolaire.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
